4a957980d7af6a64e7993f9fc858d67322346959
jiahua <1530784218@qq.com>
2025-12-04 00:21:06 +0800

    add main grpo flow

--- cs336_alignment/grpo.py
+++ cs336_alignment/grpo.py
@@ -1,14 +1,17 @@
+from datetime import datetime
+
 import torch
 from typing import Literal, Optional, Callable, Dict, List
 
+import wandb
 from torch.optim import AdamW
-from transformers import AutoTokenizer, PreTrainedModel, PreTrainedTokenizer
+from transformers import AutoTokenizer, PreTrainedModel, PreTrainedTokenizer, AutoModelForCausalLM
 from vllm import LLM, SamplingParams
-import copy
 from .initializer import VLLMInitializer
 from .gen_prompt import PromptDataset
 from .sft_helper import tokenize_prompt_and_output, get_response_log_probs
 from .grpo_helper import compute_group_normalized_rewards, grpo_microbatch_train_step
+from .drgrpo_grader import r1_zero_reward_fn
 
 class Grpo:
     def __init__(
@@ -80,8 +83,6 @@ class Grpo:
             betas=(0.9, 0.95),
         )
 
-        # ToDo: init wandb
-
     def train(self):
         for step in range(self.n_grpo_steps):
             # Sample a batch of questions D_b from D
@@ -95,7 +96,7 @@ class Grpo:
 
             repeated_prompts: List[str] = []
             responses: List[str] = []
-            old_log_probs: List[List[float]] = []   # ToDo
+            old_log_probs: List[List[float]] = []   # ToDo: reinforce_with_baseline do not need
             for completion in outputs:
                 repeated_prompts.extend([completion.prompt for _ in completion.outputs])
                 responses.extend([res.text for res in completion.outputs])
@@ -145,9 +146,51 @@ class Grpo:
                         loss_type=self.loss_type,
                         raw_rewards=micro_rewards,
                         advantages=micro_advantages,
-                        old_log_probs=old_log_probs, # ToDo
+                        # ToDo: reinforce_with_baseline do not need
+                        # old_log_probs=old_log_probs,
                     )
 
+                    # do step for each gradient_accumulation_steps
                     if (micro_step + 1) % self.gradient_accumulation_steps == 0:
                         self.optimizer.step()
                         self.optimizer.zero_grad()
+
+
+if __name__ == "__main__":
+    wandb.init(
+        project="cs336_assignment5",
+        name="grpo" + datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    )
+
+    # init policy
+    model_path = "./data/models/Qwen2.5-Math-1.5B"
+    policy = AutoModelForCausalLM.from_pretrained(
+        model_path,
+        # torch_dtype=torch.bfloat16,
+        # attn_implementation="flash_attention_2",
+    )
+    tokenizer = AutoTokenizer.from_pretrained(model_path)
+    wandb.watch(policy, log="all")
+
+    # init vllm
+    inf_vllm = VLLMInitializer(
+        model_id=model_path,
+        device="cpu",   # ToDo: compensate
+        seed=666,
+    )
+
+    # ToDo: gen questions and corresponding truths
+
+
+    # grpo train
+    grpo = Grpo(
+        policy=policy,
+        old_policy_wrapper=inf_vllm,
+        reward_fn=r1_zero_reward_fn,
+        raw_questions=["What is the capital of China?"],
+        raw_ground_truths=["What is the capital of China?"],
+        tokenizer=tokenizer,
+    )
+    grpo.train()
+
+    wandb.finish()
